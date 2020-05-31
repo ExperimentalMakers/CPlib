e9d5aa258d11130250ce95e668a0e7f20cb97ab3
cesilko1 <wilhelm.master@gmail.com>
2020-05-31 16:23:26 +0200

    update

--- Panel/Panel.h
+++ Panel/Panel.h
@@ -1,3 +1,19 @@
 /*
-Library for control panel
+	Panel.h - Library for control panel.
+
+	Version: 0.1.0
+	(C) Vilém Raška & EM, May 31, 2020.
+
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the version 3 GNU General Public License as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
